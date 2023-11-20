492e5413e861a6e27e28e583bbf8019adb95bd74
raquelbcarballo <raquelbc2996@gmail.com>
2023-11-20 21:23:44 +0100

    add recipe model

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recepies: Recipe[] = [
+    new Recipe('Tortilla', 'Tortilla de patatas con cebolla', '../../assets/totilla.png')
+  ];
 
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 }
