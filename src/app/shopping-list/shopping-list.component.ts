68b6a65314ee5ecf3616233c3a7e8d5d9169e80c
raquelbcarballo <raquelbc2996@gmail.com>
2023-11-24 17:40:48 +0100

    add shoping list component and edit component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Potatoes', 5),
+    new Ingredient('Eggs', 10),
+    new Ingredient('Onion', 1)
+  ]
 
+  constructor() { }
+
+  ngOnInit() { }
 }
