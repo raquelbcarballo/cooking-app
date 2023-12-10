909a6375a7317ea00eef1e03eb2fe506a9f03f45
raquelbcarballo <raquelbc2996@gmail.com>
2023-12-10 12:19:12 +0100

    passing data with event and property binding combined

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
   styleUrl: './recipe-book.component.css'
 })
-export class RecipeBookComponent {
+export class RecipeBookComponent implements OnInit {
+  selectedRecipe: Recipe;
 
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,12 +9,17 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Tortilla', 'Tortilla de patatas con cebolla', '../../../assets/tortilla.png'),
-    new Recipe('Tortilla', 'Tortilla de patatas con cebolla', '../../../assets/tortilla.png')
+    new Recipe('Another Tortilla', 'Tortilla de patatas con cebolla', '../../../assets/tortilla.png')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 }
